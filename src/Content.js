8feff6a016edec0b9d73e53ff95b8e97c83c1dc1
AishaSK <aishasuhail2005@gmail.com>
2023-07-26 20:14:55 +0400

    Shifted functions

--- src/Content.js
+++ src/Content.js
@@ -2,39 +2,7 @@ import React from 'react'
 import {useState} from 'react'
 import {FaTrashAlt} from 'react-icons/fa'
 
-const Content = () => {
-  const [items, setItems] = useState([
-    {
-      id: 1,
-      checked: false,
-      item: "item 1" },
-
-    {
-      id: 2,
-      checked: false,
-      item: "item 2"
-    },
-    {
-      id : 3,
-      checked: false,
-      item: "item 3"
-    }
-    
-  ])
-
-  const handleCheck = (id) => {
-    const listItems = items.map((item)=> 
-    item.id === id ? {...item, checked: !item.checked} : item)
-    setItems(listItems)
-    localStorage.setItem('shoppinglist', JSON.stringify(listItems))
-  }
-
-  const handleDelete = (id)=> {
-    const listItems = items.filter((item)=> item.id !== id)
-    setItems(listItems)
-    localStorage.setItem('shoppinglist', JSON.stringify(listItems))
-  }
-
+const Content = ({items, handleCheck, handleDelete}) => {
 
 
   return (
--- src/header.js
+++ src/header.js
@@ -1,13 +1,14 @@
 import React from 'react'
 
-const Header = (props) => {
+const Header = ({title}) => {
  
 
   return (
     <header>
-        <h1>{props.title}</h1>
+        <h1>{title}</h1>
     </header> 
   )
 }
+
 
 export default Header
