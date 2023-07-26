826dbac855ecc085a38cd1eb502457a1f5a708d3
AishaSK <aishasuhail2005@gmail.com>
2023-07-26 19:29:28 +0400

    Initial commit

---
+++ src/Content.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import {useState} from 'react'
+import {FaTrashAlt} from 'react-icons/fa'
+
+const Content = () => {
+  const [items, setItems] = useState([
+    {
+      id: 1,
+      checked: true,
+      item: "item 1" },
+
+    {
+      id: 2,
+      checked: false,
+      item: "item 2"
+    },
+    {
+      id : 3,
+      checked: false,
+      item: "item 3"
+    }
+    
+  ])
+
+  const handleCheck = (id) => {
+    const listItems = items.map((item)=> 
+    item.id === id ? {...item, checked: !item.checked} : item)
+  }
+
+
+
+  return (
+    <main>
+      <ul>
+        {items.map((item)=>(
+          <li className="item" key={item.id}>
+            <input
+              type = "checkbox"
+              onChange = {()=>handleCheck(item.id)}
+              checked={item.checked}
+              />
+            <label>{item.item}</label>
+            <FaTrashAlt 
+              role = "button" 
+              tabIndex = "0"
+             />
+          </li>
+        ))}
+      </ul>
+        
+    </main>
+  )
+}
+
+export default Content
