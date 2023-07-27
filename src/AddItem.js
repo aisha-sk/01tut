7224db6e6fe27bf448effc47c3ae09a719757a51
AishaSK <aishasuhail2005@gmail.com>
2023-07-27 14:19:51 +0400

    Update input css

--- src/AddItem.js
+++ src/AddItem.js
@@ -4,7 +4,7 @@ import {FaPlus} from 'react-icons/fa'
 const AddItem = () => {
   return (
     <form className='addForm'>
-        <label htmlFor='addItem'>Add Item</label>
+        {/* <label htmlFor='addItem'>Add Item</label> */}
         <input
          autoFocus
          id='addItem'
@@ -17,6 +17,7 @@ const AddItem = () => {
          aria-label='Add Item'
         >
             <FaPlus />
+            
         </button>
     </form>
   )
